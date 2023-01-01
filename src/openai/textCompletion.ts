fcc09cb25bac879c38bc87d0910624f965beab53
Benjamin Lim <keymote@gmail.com>
2023-01-01 01:03:03 -0800

    Remove and ignore node modules

--- src/openai/textCompletion.ts
+++ src/openai/textCompletion.ts
@@ -4,3 +4,16 @@ const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
+
+
+
+async function completeText(text: string): Promise<string> {
+
+  const completion = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: text,
+      temperature: 0.6,
+    });
+
+    return completion.data.choices[0].text ?? "";
+}
