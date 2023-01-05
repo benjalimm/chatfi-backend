cd5f230bbf2b62d0c5e59a247f57ee268ffdde0b
Benjamin Lim <keymote@gmail.com>
2023-01-04 23:04:11 -0800

    Added cors

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express, { Express, Request, Response } from 'express';
 import * as bodyParser from 'body-parser';
-
+import cors from 'cors';
 import dotenv from 'dotenv';
 import OpenAIController from './controllers/OpenAIController';
 import MockFinancialStatementManager from './controllers/MockFinancialStatementManager';
@@ -18,8 +18,9 @@ const mockFinancialStatementManager =
 new MockFinancialStatementManager(openAIController);
 
 // Body parser middleware
+app.use(cors())
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 
 app.get('/', (req: Request, res: Response) => {
@@ -28,9 +29,14 @@ app.get('/', (req: Request, res: Response) => {
 
 app.post('/', async (req: Request, res: Response) => {
   const query = req.body.query
-  console.log(`KEY: ${process.env.OPENAI_API_KEY}`)
+  console.log("BODY:")
+  console.log(req.body)
 
   try {
+
+    if (query === undefined) {
+      throw new Error('Query is undefined')
+    }
 
     // 1. This here is a large string of financial statement(s) as stringified JSON
     const documentJsonStrings = await mockFinancialStatementManager.getDocumentStringsFromQuery(query);
